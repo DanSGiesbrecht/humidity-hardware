9d3294ca628a5581661a31c5345a0102d75d9542
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2019-11-09 19:45:00 -0700

    Fix lib-table locations
    - Update sym/fp-lib-table paths in script
    - Add to .gitignore

--- pcb-source/setup-libs.sh
+++ pcb-source/setup-libs.sh
@@ -4,3 +4,7 @@ git submodule update --init
 # Copy library tables
 cp libs/kicad-symbols/sym-lib-table .
 cp libs/kicad-footprints/fp-lib-table .
+
+# Change Library Table Locations
+sed -i "s|\${KICAD_SYMBOL_DIR}|$(pwd)/libs/kicad-symbols|g" sym-lib-table
+sed -i "s|\${KISYSMOD}|$(pwd)/libs/kicad-footprints|g" fp-lib-table
