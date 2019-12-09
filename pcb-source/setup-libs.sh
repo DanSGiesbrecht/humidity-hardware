ff475423e490c2805e29d30d4a695ddd02f522db
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2019-12-08 20:08:25 -0700

    Generate modified footprint library
    - Altered 3D filepath
    - Add generated directory to .gitignore

--- pcb-source/setup-libs.sh
+++ pcb-source/setup-libs.sh
@@ -5,6 +5,17 @@ git submodule update --init
 cp libs/kicad-symbols/sym-lib-table .
 cp libs/kicad-footprints/fp-lib-table .
 
+# Copy footprint library
+rm -rf libs/gen-kicad-footprints/
+cp -r libs/kicad-footprints/ libs/gen-kicad-footprints/
+# Delete repo-related files (anything except footprint libs)
+find libs/gen-kicad-footprints/ -maxdepth 1 -type f ! -name '*.pretty' -delete
+
 # Change Library Table Locations
 sed -i "s|\${KICAD_SYMBOL_DIR}|$(pwd)/libs/kicad-symbols|g" sym-lib-table
-sed -i "s|\${KISYSMOD}|$(pwd)/libs/kicad-footprints|g" fp-lib-table
+sed -i "s|\${KISYSMOD}|$(pwd)/libs/gen-kicad-footprints|g" fp-lib-table
+
+# Change 3D model path for footprints
+for file in libs/gen-kicad-footprints/*.pretty/*; do
+    sed -i "s|\${KISYS3DMOD}|$(pwd)/libs/kicad-packages3D|g" $file
+done
