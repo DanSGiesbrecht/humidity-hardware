61ff3668ceaa3f8dde5dc9ed1ba91d1380094849
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2020-01-12 10:30:46 -0700

    Add OS-path logic
    - Identify Cygwin, Mingw, Darwin (Mac OS), Linux.
    - If needed, convert sym/fp-lib-table, 3D model paths to
        Windows-friendly paths ( /c/ -> c:/ ).

--- pcb-source/setup-libs.sh
+++ pcb-source/setup-libs.sh
@@ -1,21 +1,78 @@
+# KiCad Library Setup Tool
+#
+# Tested with: KiCad 5.15
+# Simple Setup Tool, to allow "plug-and-play" setup of projects,
+# without the need to modify KiCad environment variables.
+
+printf "
+ _   ___ _____           _ 
+| | / (_)  __ \         | |
+| |/ / _| /  \/ __ _  __| |
+|    \| | |    / _  |/ _  |
+| |\  \ | \__/\ (_| | (_| |
+\_| \_/_|\____/\__,_|\__,_|
+
+Library Setup Tool
+Copyright \x00\xC2\xA9 2020 Daniel Giesbrecht\n\n"
+
+os_path=$(pwd)
+unameOut="$(uname -s)"
+
+case "${unameOut}" in
+    Linux*)    machine=Linux;;
+    Darwin*)   machine=Mac;;
+    CYGWIN*)   machine=Cygwin;;
+    MINGW*)    machine=Mingw;;
+    *)         machine="UNKNOWN:${unameOut}"
+esac
+
+if [ $machine != Linux ] && [ $machine != Mac ]; then
+    read -r -p "Are you using Windows OS? [Y/n] " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            printf ">>> Using Windows-style filepaths\n\n"
+            os_path=$(sed 's|^.||g' <<< "$os_path")       # trim the first '/'
+            os_path=$(sed 's|^.|&:|g' <<< "$os_path")     # add ':' after the drive letter (for Windows)
+            ;;
+        *)
+            printf ">>> Unsure which OS-path to use for: "
+            echo $(uname)
+            printf "\n"
+            exit
+            ;;
+    esac
+fi
+
 # Update submodules
+printf "Updating submodules...\n"
 git submodule update --init
 
 # Copy library tables
+printf "Moving library tables...\n"
 cp libs/kicad-symbols/sym-lib-table .
 cp libs/kicad-footprints/fp-lib-table .
 
 # Copy footprint library
+printf "Generating footprint library...\n"
 rm -rf libs/gen-kicad-footprints/
 cp -r libs/kicad-footprints/ libs/gen-kicad-footprints/
 # Delete repo-related files (anything except footprint libs)
 find libs/gen-kicad-footprints/ -maxdepth 1 -type f ! -name '*.pretty' -delete
 
-# Change Library Table Locations
-sed -i "s|\${KICAD_SYMBOL_DIR}|$(pwd)/libs/kicad-symbols|g" sym-lib-table
-sed -i "s|\${KISYSMOD}|$(pwd)/libs/gen-kicad-footprints|g" fp-lib-table
+# Change Library Table Paths
+printf "Updating library tables...\n"
+sed -i "s|\${KICAD_SYMBOL_DIR}|$os_path/libs/kicad-symbols|g" sym-lib-table
+sed -i "s|\${KISYSMOD}|$os_path/libs/gen-kicad-footprints|g" fp-lib-table
 
 # Change 3D model path for footprints
+printf "Updating 3D model paths...\n"
+var=0
 for file in libs/gen-kicad-footprints/*.pretty/*; do
-    sed -i "s|\${KISYS3DMOD}|$(pwd)/libs/kicad-packages3D|g" $file
+    if [[ $((var % 2)) = 0 ]]; then
+        sed -i "s|\${KISYS3DMOD}|$os_path/libs/kicad-packages3D|g" $file &
+    else
+        sed -i "s|\${KISYS3DMOD}|$os_path/libs/kicad-packages3D|g" $file
+    fi
+    var=$((var+1))
 done
+printf "\nDONE"
